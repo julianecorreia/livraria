6a228a1775f59b375feb346ff28f534f4d9d5cd4
julianecorreia <juliane.correia@prof.unipar.br>
2025-04-29 19:21:20 -0300

    first commit

--- src/main/java/br/unipar/programacaoweb/livraria/controller/LivroController.java
+++ src/main/java/br/unipar/programacaoweb/livraria/controller/LivroController.java
@@ -2,11 +2,8 @@ package br.unipar.programacaoweb.livraria.controller;
 
 import br.unipar.programacaoweb.livraria.model.Livro;
 import br.unipar.programacaoweb.livraria.service.LivroService;
-import jakarta.validation.Valid;
-import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,16 +23,6 @@ public class LivroController {
         return ResponseEntity.status(HttpStatus.OK).body(livroService.listarTodos());
     }
 
-    @GetMapping("/livro/listar/{id}")
-    public ResponseEntity<Livro> listarLivro(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(livroService.buscarPorId(id));
-    }
-
-    @PostMapping("/livro/salvar")
-    public ResponseEntity<Livro> criarLivro(@Valid @RequestBody Livro livro) {
-        Livro livroNovo = livroService.salvar(livro);
-        return ResponseEntity.status(HttpStatus.CREATED).body(livroNovo);
-    }
 
 
 }
